4ff3047d284c1e7d0ad1d9ded72fe8ab384b4f83
pajlotapps <pajlotapps@gmail.com>
2023-08-04 20:55:43 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Be <code>onTime</code> 🐡
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/pajlotapps"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          PajlotApps
         </a>
       </header>
     </div>
